22ff83b42a5c7f6ec2d47f77d844128ec73726c7
ztc1997 <2455484588@qq.com>
2016-03-17 12:31:53 +0800

    Update Copyright

--- app/src/main/java/ztc1997/hideablenavbar/View/KeyButtonView.java
+++ app/src/main/java/ztc1997/hideablenavbar/View/KeyButtonView.java
@@ -1,18 +1,17 @@
 /*
- * Copyright (C) 2008 The Android Open Source Project
- * Copyright (C) 2015 Peter Gregus for GravityBox Project (C3C076@xda)
+ * Copyright 2015-2016 Alex Zhang aka. ztc1997
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package ztc1997.hideablenavbar.view;
@@ -35,8 +34,6 @@ import android.view.ViewConfiguration;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.ImageView;
 
-import java.lang.Math;
-
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 
@@ -44,7 +41,6 @@ public class KeyButtonView extends ImageView {
     private static final String TAG = "StatusBar.KeyButtonView";
     private static final boolean DEBUG = false;
 
-    // TODO: Get rid of this
     public static final float DEFAULT_QUIESCENT_ALPHA = 1f;
 
     private long mDownTime;
